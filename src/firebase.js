43ecf6b3d0a2c2d1c8b6fa9cb7e5c59abbe94c16
Howard Tseng <howardt12345@gmail.com>
2020-06-16 18:08:29 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,16 +3,6 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCWIxXQ7u9VbNQRg21pOwRYd1p5i7I0xCI",
-  authDomain: "portfolio-49b69.firebaseapp.com",
-  databaseURL: "https://portfolio-49b69.firebaseio.com",
-  projectId: "portfolio-49b69",
-  storageBucket: "portfolio-49b69.appspot.com",
-  messagingSenderId: "1036625872430",
-  appId: "1:1036625872430:web:46c11c7fdbd73b6b8e81bf",
-  measurementId: "G-1RHVFTPQMT"
-};
 
 firebase.initializeApp(firebaseConfig);
 
