9532aff564535d599f6117ce28adef6f70060ab9
Howard Tseng <howardt12345@gmail.com>
2020-06-19 13:38:03 -0400

    Temp main page

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
-import { auth } from '@firebase-api';
+import { auth } from '@firebase-api'
+import { mixins, Main } from '@styles';
+
+const StyledContainer = styled(Main)`
+  ${mixins.flexCenter};
+  flex-direction: column;
+`;
+
+const SigninButton = styled.button`
+  ${mixins.bigButton}
+`;
+
  
-const MainPage = () => {
-  return (
-    <div>
-      <h1>Main Page</h1>
-      <button onClick={event => {
-        auth.signOut();
-      }}>Sign Out</button>
-    </div>
-  );
+class MainPage extends Component {
+
+  render() {
+    return (
+      <StyledContainer>
+        <h1>Website Manager</h1>
+        <h4>Main Page</h4>
+        <SigninButton onClick={() => {
+          auth.signOut();
+        }}>Sign Out</SigninButton>
+      </StyledContainer>
+    );
+  }
 }
  
 export default MainPage;
