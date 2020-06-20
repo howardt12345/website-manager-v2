e4b555c93f01debb6e7066f994f4503787001081
Howard Tseng <howardt12345@gmail.com>
2020-06-20 13:54:01 -0400

    Added vars file

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,9 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
+import { vars } from '@vars';
 
+const { firebaseConfig } = vars;
 
 firebase.initializeApp(firebaseConfig);
 
