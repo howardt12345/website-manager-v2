7593030fd97ba1b075303dee5cf3d944e8179e76
Howard Tseng <howardt12345@gmail.com>
2020-06-25 13:14:39 -0400

    Update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { auth } from '@firebase-api'
+import { mixins, Main, Title, theme } from '@styles';
+import { vars } from '@vars';
 
-const SigninButton = styled.button`
+const { email, pass } = vars;
+const { colors, fonts, fontSizes } = theme;
 
+const StyledContainer = styled(Main)`
+  ${mixins.flexCenter};
+  flex-direction: column;
 `;
 
- 
+const SigninButton = styled.button`
+  ${mixins.bigButton}
+  margin-top: 20px;
+`;
+
+
 class LoginPage extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Login Page</h1>
-        <SigninButton onClick={event => {
-          auth.signInWithEmailAndPassword('aaa', 'aaa')
+      <StyledContainer>
+        <Title>Website Manager</Title>
+        <h4>Login Page</h4>
+        <SigninButton onClick={() => {
+          auth.signInWithEmailAndPassword(email, pass)
         }}>Sign In</SigninButton>
-      </div>
+      </StyledContainer>
     );
   }
 }
