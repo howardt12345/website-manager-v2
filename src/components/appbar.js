418c9215b53f72569c33a69b46fc160f10b9833d
Howard Tseng <howardt12345@gmail.com>
2020-09-04 19:33:32 -0400

    Highlighting for current page

--- src/components/appbar.js
+++ src/components/appbar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { MainConsumer, ThemeToggleConsumer } from '@api';
 import {
   AppBar,
+  Box,
   Grid,
   IconButton,
   Link,
@@ -12,6 +13,12 @@ import { ArrowBack, Brightness3, Brightness7 } from '@material-ui/icons'
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
+  appBar: {
+    position: 'fixed',
+    color: 'default',
+    zIndex: theme.zIndex.drawer + 1,
+    boxShadow: 'none',
+  },
   nav: {
     justifyContent: 'center',
   },
@@ -24,74 +31,81 @@ class MainAppBar extends Component {
 
   render() {
     const classes = this.props.classes;
+    const page = this.props.page;
 
     return (
       <MainConsumer>
        {maincontext => (
-        <div height='56px'>
-          <AppBar position='fixed' color='default' elevation={0}>
-            <Toolbar>
-              <IconButton 
-                edge="start" 
-                className={classes.menuButton} 
-                color="inherit" 
-                onClick={() => {
-                  maincontext.setPage('index');
-                }} 
-              >
-                <ArrowBack />
-              </IconButton>
-              <Grid container className={classes.nav} spacing={6}>
-                <Grid item>
-                  <Typography>
+        <AppBar color='default' className={classes.appBar}>
+          <Toolbar variant="dense">
+            <IconButton 
+              edge="start" 
+              className={classes.menuButton} 
+              color="inherit" 
+              onClick={() => {
+                maincontext.setPage('index');
+              }} 
+            >
+              <ArrowBack />
+            </IconButton>
+            <Grid container className={classes.nav} spacing={6}>
+              <Grid item>
+                <Typography>
+                  <Box fontWeight={ page === 'portfolio' ? 'fontWeightBold' : 'fontWeightRegular' }>
                     <Link 
                       href="#" 
                       onClick={() => {
-                        maincontext.setPage('portfolio');
+                        if(maincontext.page !== 'portfolio') {
+                          maincontext.setPage('portfolio');
+                        }
                       }} 
                       color="inherit"
                     >
                       Portfolio
                     </Link>
-                  </Typography>
-                </Grid>
-                <Grid item>
-                  <Typography>
+                  </Box>
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography>
+                  <Box fontWeight={ page === 'messages' ? 'fontWeightBold' : 'fontWeightRegular' }>
                     <Link 
                       href="#" 
                       onClick={() => {
-                        maincontext.setPage('messages');
+                        if(maincontext.page !== 'messages') {
+                          maincontext.setPage('messages');
+                        }
                       }} 
                       color="inherit"
                     >
                       Messages
                     </Link>
-                  </Typography>
-                </Grid>
+                  </Box>
+                </Typography>
               </Grid>
-              <ThemeToggleConsumer>
-                {themeContext => (
-                  <IconButton 
-                    className={classes.toggleButton} 
-                    color ='inherit'
-                    onClick={themeContext.toggleTheme}
-                  >
-                    {themeContext.dark ? <Brightness3 /> : <Brightness7 />}
-                  </IconButton>
-                )}
-              </ThemeToggleConsumer>
-            </Toolbar>
-          </AppBar>
-        </div>
+            </Grid>
+            <ThemeToggleConsumer>
+              {themeContext => (
+                <IconButton 
+                  className={classes.toggleButton} 
+                  color ='inherit'
+                  onClick={themeContext.toggleTheme}
+                >
+                  {themeContext.dark ? <Brightness3 /> : <Brightness7 />}
+                </IconButton>
+              )}
+            </ThemeToggleConsumer>
+          </Toolbar>
+        </AppBar>
        )} 
       </MainConsumer>
     )
   }
 }
 
-export default () => {
+export default (props) => {
   const classes = useStyles();
   return (
-    <MainAppBar classes={classes} />
+    <MainAppBar classes={classes} {...props}/>
   )
 }
